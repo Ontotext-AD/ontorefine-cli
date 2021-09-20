1398b6c8c2c77613cba1b014e22b5d9523cfbd54
Antoniy Kunchev <antoniy.kunchev@ontotext.com>
2021-09-20 19:40:03 +0300

    GDB-6097 Moves the CLI part of the ontorefine-client repository in here (#1)
    - Moved all of the related logic and dependencies from
    `ontorefine-client` repository in this one. This is done in order to
    separate code base in two different parts. One will be a generic
    library, which provides a communication point for the OntoRefine tool
    and the other is the actual command-line interface, which needs to be
    integrated within GDB for easy usage.
    - Added .git config files.
    - Added git workflow for building and verifying the code base quality.

---
+++ src/main/java/com/ontotext/refine/cli/Export.java
@@ -0,0 +1,84 @@
+package com.ontotext.refine.cli;
+
+import com.ontotext.refine.client.RefineClient;
+import com.ontotext.refine.client.command.ExportRowsResponse;
+import com.ontotext.refine.client.command.RefineCommands;
+import com.ontotext.refine.client.exceptions.RefineException;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.FileUtils;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.ExitCode;
+import picocli.CommandLine.Parameters;
+
+
+/**
+ * Defines the export process for different formats and all of the required arguments for it.
+ *
+ * @author Antoniy Kunchev
+ */
+@Command(
+    name = "export",
+    description = "Exports the data of specified project in specific format.",
+    version = "1.0.0",
+    mixinStandardHelpOptions = true)
+class Export extends Process {
+
+  @Parameters(
+      index = "0",
+      paramLabel = "PROJECT",
+      description = "The identifier of the project to which the data should be exported.")
+  private String project;
+
+  @Parameters(
+      index = "1",
+      paramLabel = "FORMAT",
+      description = "The output format in which the data should be exported.")
+  private String format;
+
+  @Override
+  public Integer call() throws Exception {
+    if (!isFormatSupported()) {
+      System.err.println(String.format("The format: '%s' is currently not supported.", format));
+      return ExitCode.USAGE;
+    }
+
+    File result = null;
+    try (RefineClient client = getClient()) {
+
+      ExportRowsResponse response = RefineCommands
+          .exportRows()
+          .project(project)
+          .format(format)
+          // TODO not sure if there are more options for the engine
+          .engine("{\"mode\":\"row-based\"}")
+          .token(getToken())
+          .build()
+          .execute(client);
+
+      result = response.getFile();
+
+      System.out.println(FileUtils.readFileToString(result, StandardCharsets.UTF_8));
+      return ExitCode.OK;
+    } catch (RefineException re) {
+      System.err.println(re.getMessage());
+    } finally {
+      FileUtils.deleteQuietly(result);
+    }
+    return ExitCode.SOFTWARE;
+  }
+
+  private boolean isFormatSupported() {
+    return "csv".equalsIgnoreCase(format) || "json".equalsIgnoreCase(format);
+  }
+
+  /**
+   * Runs the export command.
+   *
+   * @param args to pass to the process
+   */
+  public static void main(String[] args) {
+    System.exit(new CommandLine(new Export()).execute(args));
+  }
+}
---
+++ src/test/java/com/ontotext/refine/cli/ExportTest.java
@@ -0,0 +1,128 @@
+package com.ontotext.refine.cli;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.ontotext.refine.cli.RefineResponder.HandlerContext;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import org.apache.http.HttpStatus;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.protocol.HttpRequestHandler;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine.ExitCode;
+
+
+/**
+ * Test for {@link Export}.
+ *
+ * @author Antoniy Kunchev
+ */
+class ExportTest extends BaseProcessTest {
+
+  private static RefineResponder responder = new RefineResponder();
+  private static boolean shouldFailOperationsExtraction;
+
+  @BeforeAll
+  static void beforeAll() throws IOException {
+    shouldFailOperationsExtraction = false;
+    responder.start(mockResponses());
+  }
+
+  @AfterAll
+  static void cleanup() {
+    responder.stop();
+  }
+
+  @Override
+  protected Consumer<String[]> commandExecutor() {
+    return Export::main;
+  }
+
+  @Test
+  @ExpectSystemExit(ExitCode.USAGE)
+  void shouldExitWithErrorOnMissingProjectArg() {
+    try {
+      commandExecutor().accept(args("-u " + responder.getUri()));
+    } finally {
+      assertTrue(assertMissingArgError().contains("PROJECT"));
+    }
+  }
+
+  @Test
+  @ExpectSystemExit(ExitCode.USAGE)
+  void shouldExitWithErrorOnMissingFormatArg() {
+    try {
+      commandExecutor().accept(args(PROJECT_ID, "-u " + responder.getUri()));
+    } finally {
+      assertTrue(assertMissingArgError().contains("FORMAT"));
+    }
+  }
+
+  @Test
+  @ExpectSystemExit(ExitCode.USAGE)
+  void shouldExitWithErrorOnInvalidFormatArg() {
+    try {
+      commandExecutor().accept(args(PROJECT_ID, "xml", "-u " + responder.getUri()));
+    } finally {
+      assertEquals(
+          "The format: 'xml' is currently not supported.",
+          consoleErrors().stripTrailing());
+    }
+  }
+
+  @Test
+  @ExpectSystemExit(ExitCode.SOFTWARE)
+  void shouldFailDuringOperationsExtraction() {
+    try {
+      shouldFailOperationsExtraction = true;
+
+      commandExecutor().accept(args(PROJECT_ID, "csv", "-u " + responder.getUri()));
+    } finally {
+      shouldFailOperationsExtraction = false;
+
+      assertEquals(
+          "Failed to export data for project: '1812661014997' due to: 'Connection reset'",
+          consoleErrors().stripTrailing());
+    }
+  }
+
+  @Test
+  @ExpectSystemExit(ExitCode.OK)
+  void shouldPassSuccessfully() throws IOException {
+    try {
+      commandExecutor().accept(args(PROJECT_ID, "csv", "-u " + responder.getUri()));
+    } finally {
+      String errors = consoleErrors();
+      assertTrue(errors.isEmpty(), "Expected no errors but there were: " + errors);
+
+      assertEquals("CSV-content", consoleOutput().stripTrailing());
+    }
+  }
+
+  private static Map<String, HttpRequestHandler> mockResponses() {
+    Map<String, HttpRequestHandler> responses = new HashMap<>();
+    HandlerContext context = new HandlerContext();
+    responses.put("/orefine/command/core/get-csrf-token", RefineResponder.csrfToken(context));
+    responses.put("/orefine/command/core/export-rows", exportHandler());
+    return responses;
+  }
+
+  private static HttpRequestHandler exportHandler() {
+    return (request, response, context) -> {
+      if (shouldFailOperationsExtraction) {
+        throw new RuntimeException();
+      } else {
+        response.setStatusCode(HttpStatus.SC_OK);
+        BasicHttpEntity entity = new BasicHttpEntity();
+        entity.setContent(new ByteArrayInputStream("CSV-content".getBytes()));
+        response.setEntity(entity);
+      }
+    };
+  }
+}

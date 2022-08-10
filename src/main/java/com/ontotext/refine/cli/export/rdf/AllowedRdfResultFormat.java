79ea88d09017ecbd1085a41ae888174c7339cd51
Antoniy Kunchev <antoniy.kunchev@ontotext.com>
2022-08-10 14:45:06 +0300

    Removes couple of the allowed RDF result formats
    - Removed `ResultFormat.HDT` and `ResultFormat.RDFA` from the allowed
    RDF result formats, because currently they are not supported in the
    Ontotext Refine and it is misleading to have them as potential values
    for the arguments.

--- src/main/java/com/ontotext/refine/cli/export/rdf/AllowedRdfResultFormat.java
+++ src/main/java/com/ontotext/refine/cli/export/rdf/AllowedRdfResultFormat.java
@@ -3,6 +3,7 @@ package com.ontotext.refine.cli.export.rdf;
 import com.ontotext.refine.client.command.rdf.ResultFormat;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Provides the values for the allowed RDF result formats and the completion candidates for the RDF
@@ -12,9 +13,12 @@ import java.util.Iterator;
  */
 public class AllowedRdfResultFormat implements Iterable<String> {
 
+  private static final Set<ResultFormat> UNSUPPORTED = Set.of(ResultFormat.HDT, ResultFormat.RDFA);
+
   @Override
   public Iterator<String> iterator() {
     return Arrays.stream(ResultFormat.values())
+        .filter(value -> !UNSUPPORTED.contains(value))
         .map(value -> value.toString().toLowerCase())
         .iterator();
   }
